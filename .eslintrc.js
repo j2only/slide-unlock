3c64694249e0d936e05bc90b34d6d7319ee359fa
Anton Zolotov <antonzolotov@outlook.com>
2022-01-27 15:57:33 +0500

    Remove "@vue/eslint-config-standard"

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,7 @@ module.exports = {
     },
     extends: [
         "plugin:vue/vue3-essential",
-        "eslint:recommended",
-        "@vue/standard"
+        "eslint:recommended"
     ],
     parserOptions: {
         ecmaVersion: 2020
